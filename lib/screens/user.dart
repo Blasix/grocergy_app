de31b63a0634c33be6c015884d9ebffb82bb7a4d
BlasixPC <blasixcorp@gmail.com>
2022-07-29 22:13:08 +0200

    implement user screen

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,12 +1,143 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
 
-class UserScreen extends StatelessWidget {
+import '../provider/dark_theme_provider.dart';
+import '../widgets/text_widget.dart';
+
+class UserScreen extends StatefulWidget {
   const UserScreen({Key? key}) : super(key: key);
 
   @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("User Screen")),
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'Hi, ',
+                    style: TextStyle(
+                      color: color,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: const <TextSpan>[
+                      TextSpan(
+                        text: 'Name',
+                      )
+                    ],
+                  ),
+                ),
+                TextWidget(
+                  text: 'Email',
+                  color: color,
+                  textSize: 18,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Divider(
+                  thickness: 2,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                _listTiles(
+                  title: 'Address',
+                  subtitle: 'Subtitle here',
+                  icon: IconlyLight.profile,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Orders',
+                  icon: IconlyLight.bag,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Wishlist',
+                  icon: IconlyLight.heart,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Viewed',
+                  icon: IconlyLight.show,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Forgot password',
+                  icon: IconlyLight.lock,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTiles(
+                  title: 'Logout',
+                  icon: IconlyLight.logout,
+                  onPressed: () {},
+                  color: color,
+                ),
+                SwitchListTile(
+                  activeColor:
+                      themeState.getDarkTheme ? Colors.blue[900] : Colors.blue,
+                  title: TextWidget(
+                    text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
+                    color: color,
+                    textSize: 22,
+                  ),
+                  secondary: Icon(themeState.getDarkTheme
+                      ? Icons.dark_mode_outlined
+                      : Icons.light_mode_outlined),
+                  onChanged: (bool value) {
+                    setState(() {
+                      themeState.setDarkTheme = value;
+                    });
+                  },
+                  value: themeState.getDarkTheme,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _listTiles({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    required Function onPressed,
+    required Color color,
+  }) {
+    return ListTile(
+      title: TextWidget(
+        text: title,
+        color: color,
+        textSize: 22,
+      ),
+      subtitle: Text(subtitle ?? ""),
+      leading: Icon(icon),
+      trailing: const Icon(IconlyLight.arrowRight2),
+      onTap: () => onPressed,
     );
   }
 }
