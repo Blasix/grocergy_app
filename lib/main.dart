1eece266cc998dca35f22d80573d19c6862f24d3
BlasixPC <blasixcorp@gmail.com>
2022-07-29 16:40:25 +0200

    app initialization and dark theme implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:grocery/provider/dark_theme_provider.dart';
 import 'package:grocery/screens/home.dart';
+import 'package:provider/provider.dart';
 
-import 'consts/theme_data.dart';
+import 'constants/theme_data.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: Styles.themeData(false, context),
-      home: const Home(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return themeChangeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: Styles.themeData(themeProvider.getDarkTheme, context),
+          home: const Home(),
+        );
+      }),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:grocery/provider/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    return Scaffold(
+      body: Center(
+        child: SwitchListTile(
+          title: const Text("Theme"),
+          secondary: Icon(themeState.getDarkTheme
+              ? Icons.dark_mode_outlined
+              : Icons.light_mode_outlined),
+          onChanged: (bool value) {
+            setState(() {
+              themeState.setDarkTheme = value;
+            });
+          },
+          value: themeState.getDarkTheme,
+        ),
+      ),
+    );
   }
 }
