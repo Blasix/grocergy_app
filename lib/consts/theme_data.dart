e04479bea90e930f95078a7479c44d4fab6cd279
BlasixPC <blasixcorp@gmail.com>
2022-07-29 16:04:27 +0200

    first commit

---
+++ lib/consts/theme_data.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static ThemeData themeData(bool isDarkTheme, BuildContext context) {
+    return ThemeData(
+      scaffoldBackgroundColor:
+          //0A1931  // white yellow 0xFFFCF8EC
+          isDarkTheme ? Colors.blueGrey[900] : Colors.blueGrey[50],
+      primaryColor: Colors.blue,
+      colorScheme: ThemeData().colorScheme.copyWith(
+            secondary: isDarkTheme ? Colors.blueGrey[900] : Colors.blueGrey[50],
+            brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+          ),
+      cardColor: isDarkTheme ? Colors.blueGrey[900] : Colors.blueGrey[50],
+      canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
+      buttonTheme: Theme.of(context).buttonTheme.copyWith(
+          colorScheme: isDarkTheme
+              ? const ColorScheme.dark()
+              : const ColorScheme.light()),
+    );
+  }
+}
