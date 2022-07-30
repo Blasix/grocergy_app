a8b212e153818076270ceddb559435f31fa5233f
BlasixPC <blasixcorp@gmail.com>
2022-07-30 12:51:51 +0200

    implement address and logout dialog

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:provider/provider.dart';
@@ -14,6 +13,14 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  final TextEditingController _addressTextController =
+      TextEditingController(text: "");
+  @override
+  void dispose() {
+    _addressTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
@@ -60,9 +67,11 @@ class _UserScreenState extends State<UserScreen> {
                 ),
                 _listTiles(
                   title: 'Address',
-                  subtitle: 'Subtitle here',
+                  subtitle: 'your address',
                   icon: IconlyLight.profile,
-                  onPressed: () {},
+                  onPressed: () {
+                    _showAddressDialog();
+                  },
                   color: color,
                 ),
                 _listTiles(
@@ -92,7 +101,9 @@ class _UserScreenState extends State<UserScreen> {
                 _listTiles(
                   title: 'Logout',
                   icon: IconlyLight.logout,
-                  onPressed: () {},
+                  onPressed: () {
+                    _showLogoutDialog();
+                  },
                   color: color,
                 ),
                 SwitchListTile(
@@ -121,6 +132,94 @@ class _UserScreenState extends State<UserScreen> {
     );
   }
 
+  Future<void> _showAddressDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          final themeState = Provider.of<DarkThemeProvider>(context);
+          return AlertDialog(
+            backgroundColor: themeState.getDarkTheme
+                ? Theme.of(context).cardColor
+                : Colors.white,
+            title: const Text('Update'),
+            content: TextField(
+              // onChanged: (value) {
+              //   print(
+              //       '_addressTextController.text ${_addressTextController.text}');
+              // },
+              controller: _addressTextController,
+              // maxLines: 5,
+              decoration: const InputDecoration(hintText: "Your address"),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Update',
+                  style: TextStyle(
+                    color: themeState.getDarkTheme
+                        ? Colors.blue[900]
+                        : Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> _showLogoutDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          final themeState = Provider.of<DarkThemeProvider>(context);
+          return AlertDialog(
+            backgroundColor: themeState.getDarkTheme
+                ? Theme.of(context).cardColor
+                : Colors.white,
+            title: Row(
+              children: [
+                const Text('Sign out '),
+                Image.asset(
+                  'assets/images/warning-sign.png',
+                  height: 20,
+                  width: 20,
+                  fit: BoxFit.fill,
+                )
+              ],
+            ),
+            content: const Text('Do you wanna Sing out?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  if (Navigator.canPop(context)) {
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: themeState.getDarkTheme
+                        ? Colors.blue[900]
+                        : Colors.blue,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Sing out',
+                  style: TextStyle(
+                    color:
+                        themeState.getDarkTheme ? Colors.red[900] : Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   Widget _listTiles({
     required String title,
     String? subtitle,
@@ -137,7 +236,9 @@ class _UserScreenState extends State<UserScreen> {
       subtitle: Text(subtitle ?? ""),
       leading: Icon(icon),
       trailing: const Icon(IconlyLight.arrowRight2),
-      onTap: () => onPressed,
+      onTap: () {
+        onPressed();
+      },
     );
   }
 }
